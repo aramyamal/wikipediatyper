d2cd486bc5e76f67db844a65de033b90f923f716
aramyamal <aram@aramjamal.com>
2025-02-21 22:32:55 +0100

    feat(component): add Typing component skeleton

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Col, Container, Row } from 'react-bootstrap';
 import TopBar from "./components/TopBar/TopBar";
+import Typing from "./components/Typing/Typing";
 
 const App: React.FC = () => {
     return (
         <Container className='py-1'>
             <TopBar />
+            <Typing />
         </Container>
     )
 }
