70e593500621787931bca1aeee337b33d0ff350f
aramyamal <aram@aramjamal.com>
2025-02-21 22:32:55 +0100

    feat(component): add Typing component skeleton

---
+++ client/src/components/Typing/Typing.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css"
+
+import classes from "./Typing.module.css";
+
+const Typing: React.FC = () => {
+    const testString: string = "This is a wiki article, you should read it, no, you should type it."
+
+    return (
+        <>
+        </>
+    )
+};
+
+export default Typing;
