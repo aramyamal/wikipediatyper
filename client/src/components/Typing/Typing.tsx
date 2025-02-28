56d23b54ba869e68d6a3749d1497cd17a30a53ba
aramyamal <aram@aramjamal.com>
2025-03-01 00:44:06 +0100

    feat(client): add fetching article from backend

--- client/src/components/Typing/Typing.tsx
+++ client/src/components/Typing/Typing.tsx
@@ -1,20 +1,60 @@
 import React, { useState, useRef, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css"
 
 import classes from "./Typing.module.css";
 
+interface ArticleSegment {
+    type: `header${number}` | "text",
+    body: string
+}
+
+interface Article {
+    title: string,
+    segments: ArticleSegment[]
+}
+
 const Typing: React.FC = () => {
     const [userInput, setUserInput] = useState("");
     const inputRef = useRef<HTMLInputElement | null>(null);
-    const textToType: string =
-        "This is a wiki article, you should read it, no, you should type it."
+    const [textToType, setTextToType] = useState<Article>({
+        title: "Loading...",
+        segments: []
+    });
+    const [hasFetched, setHasFetched] = useState<boolean>(false);
+
+    const location = useLocation();
+
+    useEffect(() => {
+        const fetchText = async () => {
+            if (location.pathname === "/" || hasFetched) {
+                return;
+            }
+
+            setHasFetched(true);
+
+            try {
+                const apiUrl = `http://localhost:3000${location.pathname}`;
+                const response = await axios.get<Article>(apiUrl);
+                setTextToType(response.data);
+            } catch (error) {
+                console.error("Error fetching Article:", error);
+                setTextToType({
+                    title: "Error fetching Article",
+                    segments: []
+                });
+
+            }
+        };
+
+        fetchText();
+    }, [location.pathname]); // re-fetch when URL changes
 
     const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setUserInput(e.target.value);
     }
-
-    const textArray: string[] = textToType.split("");
 
     function getTypingClass(letter: string, index: number) {
         const isLastIndex = index === userInput.length - 1;
@@ -55,8 +95,9 @@ const Typing: React.FC = () => {
                     className="opacity-0 position-absolute"
                 />
 
-                {textArray.map((letter, index) => (
-                    <span className={`${getTypingClass(letter, index)}`}>
+                {textToType.title.split("").map((letter, index) => (
+                    <span key={index}
+                        className={`${getTypingClass(letter, index)}`}>
                         {letter}
                     </span>
                 ))}
