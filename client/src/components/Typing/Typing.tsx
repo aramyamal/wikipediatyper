5db69c8755ef607a6952a5b6bcb21cd3e99ab993
aramyamal <aram@aramjamal.com>
2025-03-02 00:43:42 +0100

    feat(typing): add header styling and auto scroll

--- client/src/components/Typing/Typing.tsx
+++ client/src/components/Typing/Typing.tsx
@@ -20,6 +20,8 @@ const Typing: React.FC = () => {
     const [userInput, setUserInput] = useState<string>("");
     const [currentSegmentIndex, setCurrentSegmentIndex] = useState<number>(0)
     const inputRef = useRef<HTMLInputElement | null>(null);
+    const currentSegmentRef = useRef<HTMLDivElement | null>(null);
+
     const [article, setArticle] = useState<Article>({
         title: "Loading...",
         segments: []
@@ -52,9 +54,27 @@ const Typing: React.FC = () => {
             ? article.title
             : article.segments[currentSegmentIndex - 1].body;
 
+    const currentArticleType: string =
+        currentSegmentIndex === 0
+            ? "title"
+            : article.segments[currentSegmentIndex - 1].type;
+
+    const upcomingSegments: ArticleSegment[] =
+        currentSegmentIndex === 0
+            ? article.segments
+            : article.segments.slice(currentSegmentIndex);
+
     // auto focus when currentSegmentIndex changes
     useEffect(() => {
         inputRef.current?.focus();
+    }, [currentSegmentIndex]);
+
+    // Scroll to current segment when it updates
+    useEffect(() => {
+        currentSegmentRef.current?.scrollIntoView({
+            behavior: "smooth",
+            block: "center",  // Ensures it centers in view
+        });
     }, [currentSegmentIndex]);
 
     const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
@@ -89,34 +109,72 @@ const Typing: React.FC = () => {
         return classes.typed_wrong;
     }
 
+    function getHeaderClass(segmentType: string): string {
+        return segmentType === "text"
+            ? ""
+            : "fw-bold";
+    }
+
     return (
         <div onClick={() => inputRef.current?.focus()}>
+            {/* completed segments */}
             {currentSegmentIndex > 0 && (
-                <div className={`${classes.typed}`}>
-                    <p className="fw-bold">{article.title}<i className={`mx-2 bi bi-arrow-return-left ${classes.typed}`}></i></p>
-                    {article.segments.slice(0, currentSegmentIndex - 1).map((seg, idx) => (
-                        <p key={idx}>{seg.body}</p>
-                    ))}
+                <div className={classes.typed}>
+                    <p className="fw-bold">
+                        {article.title}
+                        <i className={`mx-2 bi bi-arrow-return-left 
+                            ${classes.to_type}`}
+                        ></i>
+                    </p>
+                    {article.segments.
+                        slice(0, currentSegmentIndex - 1)
+                        .map((seg, idx) => (
+                            <p className={`${getHeaderClass(seg.type)}`}
+                                key={idx}
+                            >
+                                {seg.body}
+                                <i className={`mx-2 bi bi-arrow-return-left 
+                                ${classes.to_type}`}
+                                ></i>
+                            </p>
+                        ))}
                 </div>
             )}
 
-            <div>
+            <div ref={currentSegmentRef}>
                 {currentText.split("").map((letter, index) => (
-                    <span key={index} className={getTypingClass(letter, index)}>
+                    <span
+                        key={index}
+                        className={` ${getHeaderClass(currentArticleType)}
+                            ${getTypingClass(letter, index)}`}
+                    >
                         {letter}
                     </span>
                 ))}
                 <i className={`mx-2 bi bi-arrow-return-left ${classes.to_type}`}></i>
             </div>
 
+            {upcomingSegments.length > 0 && (
+                <div className={classes.to_type}>
+                    {upcomingSegments.map((seg, idx) => (
+                        <p className={`${getHeaderClass(seg.type)}`}
+                            key={idx}>{seg.body}
+                            <i className={`mx-2 bi bi-arrow-return-left 
+                                ${classes.to_type}`}
+                            ></i>
+                        </p>
+                    ))}
+                </div>
+            )}
 
+            {/* invisible input to capture keystrokes */}
             <input
                 ref={inputRef}
                 type="text"
                 value={userInput}
                 onChange={handleInputChange}
                 onKeyDown={handleKeyDown}
-                className="opacity-0 position-absolute"
+                className="opacity-0 position-fixed"
             />
         </div>
     );
