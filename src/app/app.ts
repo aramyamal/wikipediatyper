9a752c9bb085d38682a91d2b3ba4964da83dc6be
aramyamal <aram@aramjamal.com>
2025-06-08 15:57:02 +0200

    feat(header): add header component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { Header } from './header/header';
 // import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   // imports: [RouterOutlet],
+  imports: [Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
