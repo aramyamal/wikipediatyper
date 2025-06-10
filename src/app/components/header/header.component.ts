13b5d742bb6c29b5778164d316b6346a7f79d4b4
aramyamal <aram@aramjamal.com>
2025-06-10 14:05:26 +0200

    feat(header): add intial layout and design without functionality

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  articleName = signal("Article Name Placeholder");
+
+  onSearchClick() { }
+  onShuffleClick() { }
 
 }
