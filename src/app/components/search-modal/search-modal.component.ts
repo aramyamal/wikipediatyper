b9b98d7e9a1dfd04c904ec2c5ef92993a0832dfb
aramyamal <aram@aramjamal.com>
2025-06-11 13:35:22 +0200

    feat(SearchModal): add initial search form

--- src/app/components/search-modal/search-modal.component.ts
+++ src/app/components/search-modal/search-modal.component.ts
@@ -1,13 +1,16 @@
 import { Component, AfterViewInit, viewChild, ElementRef } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { Modal } from 'bootstrap';
 
 @Component({
   selector: 'app-search-modal',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './search-modal.component.html',
   styleUrl: './search-modal.component.css'
 })
 export class SearchModalComponent implements AfterViewInit {
+
+  searchTerm = new FormControl("");
 
   modalElement = viewChild<ElementRef>("searchModelRef");
   private searchModal!: Modal;
